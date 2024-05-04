ed7fd211bc783c4a50b116e28532d0839129c324
Justin Harding <justin.harding@midurth.com>
2024-05-05 09:48:05 +1200

    enabled a bunch of astrocommunity plugins

--- lua/community.lua
+++ lua/community.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
@@ -9,4 +9,29 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
+  -- { import = "astrocommunity.editing-support.chatgpt-nvim" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.completion.codeium-vim" },
+  { import = "astrocommunity.motion.flash-nvim" },
+  { import = "astrocommunity.motion.mini-surround" },
+  { import = "astrocommunity.programming-language-support.csv-vim" },
+  { import = "astrocommunity.programming-language-support.nvim-jqx" },
+
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.cmake" },
+  { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.fish" },
+  { import = "astrocommunity.pack.go" },
+  --  there is a problem loading the haskell dap thing
+  --  so I copied the config to haskell.lua
+  -- { import = "astrocommunity.pack.haskell" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.sql" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.yaml" },
+  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
